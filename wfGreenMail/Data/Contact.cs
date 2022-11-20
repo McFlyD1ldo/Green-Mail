77ef8591d939c52f4383da3b526f7644637dcab1
Dan Manns-Roller <dani.mannsroller@googlemail.com>
2022-11-20 17:20:04 +0100

    added database, Contact class, database context for it and the connection string in the settings

---
+++ wfGreenMail/Data/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfGreenMail.Data
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [StringLength(50)]
+        public string? FName { get; set; }
+        
+        [StringLength(50)]
+        public string LName { get; set; }
+
+        [StringLength(50)]
+        public string Email { get; set; }
+        
+        public DateTime? Birthday { get; set; }
+        
+        
+    }
+}
---
+++ wfGreenMail/Data/ContactDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wfGreenMail.Data
+{
+    internal class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Contact>().Property(c => c.FName).IsRequired(false);
+            modelBuilder.Entity<Contact>().Property(c => c.Birthday).IsRequired(false);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            Properties.Settings settings = new();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=GreenMailDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+    }
+}
