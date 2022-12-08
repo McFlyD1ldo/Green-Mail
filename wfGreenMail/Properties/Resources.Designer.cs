b2fa4592392d66d56858b4d8787e578e03b434bf
Dan Manns-Roller <dani.mannsroller@googlemail.com>
2022-12-08 17:10:59 +0100

    added paperclip logo for attachment button

--- wfGreenMail/Properties/Resources.Designer.cs
+++ wfGreenMail/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace wfGreenMail.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap paperclip {
+            get {
+                object obj = ResourceManager.GetObject("paperclip", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
