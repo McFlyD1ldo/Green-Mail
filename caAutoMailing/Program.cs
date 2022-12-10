82d42fe78120b2187874a0f51eaf78d735cd7025
Dan Manns-Roller <dani.mannsroller@googlemail.com>
2022-12-10 14:24:45 +0100

    changed auto mail setting from parameters to ini file

--- caAutoMailing/Program.cs
+++ caAutoMailing/Program.cs
@@ -1,5 +1,4 @@
 ﻿using wfGreenMail.Data;
-using wfGreenMail;
 using clMailer;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,29 +6,35 @@ namespace caAutoMailing
 {
     internal class Program
     {
+
         static async Task Main(string[] args)
         {
+            List<string> Attachments;
             Console.WriteLine("E-mail pending...");
             ContactDBContext GreenMailDB = new();
-            ContactMailer mailer = new();
-            mailer.establishConnection(args[0], int.Parse(args[1]));
-            ContactMailer.Username = args[2];
-            ContactMailer.Password = args[3];
+            wfGreenMail.ContactMailer mailer = new();
+            ReadWriteINIfile reader = new(args[0]);
+            Attachments = reader.ReadINI("DATA", "ATTACHMENTS").Split(';').ToList();
+            mailer.establishConnection(reader.ReadINI("ACCESS", "HOST"), int.Parse(reader.ReadINI("ACCESS", "PORT")));
+            wfGreenMail.ContactMailer.Username = reader.ReadINI("ACCESS", "USER");
+            wfGreenMail.ContactMailer.Password = reader.ReadINI("ACCESS", "PASS");
             mailer.Authenticate();
             GreenMailDB.Contacts.Load();
-            var contacts = GreenMailDB.Contacts.Local.Where(c => c.Newsletter).ToList();
-            List<string> adresses = new();
-            foreach (var item in contacts)
-            {
-                adresses.Add(item.Email);
-            }
+            List<string> adresses = reader.ReadINI("DATA", "ADRESSES").Split(';').ToList();
+            adresses.Remove(adresses.Last());
             try
             {
-               await ContactMailer.sendMassMail(adresses, new EmailDto(from: ContactMailer.Username, args[4], File.ReadAllText(args[5])));
+                if (Attachments.Count > 0)
+                {
+                    await wfGreenMail.ContactMailer.sendMassMail(adresses, new EmailDto(from: wfGreenMail.ContactMailer.Username, reader.ReadINI("DATA", "SUBJECT"), File.ReadAllText(reader.ReadINI("DATA", "BODY")), Attachments));
+                }
+                else
+                {
+                    await wfGreenMail.ContactMailer.sendMassMail(adresses, new EmailDto(from: wfGreenMail.ContactMailer.Username, reader.ReadINI("DATA", "SUBJECT"), File.ReadAllText(reader.ReadINI("DATA", "BODY"))));
+                }
             }
             catch (Exception e)
             {
-
                 File.WriteAllText(Environment.CurrentDirectory + "log_" + DateTime.Now, "Error while sending: " + e.Message);
             }
         }
---
+++ clMailer/ReadWriteINIfile.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caAutoMailing
+{
+    public class ReadWriteINIfile : IDisposable
+    {
+        public string path;
+        
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string name, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public ReadWriteINIfile(string iniPath)
+        {
+            path = iniPath;
+        }
+
+        public void WriteINI(string section, string key, string value)
+        {
+            WritePrivateProfileString(section, key, value, this.path);
+        }
+
+        public string ReadINI(string section, string key)
+        {
+            StringBuilder sb = new StringBuilder(255);
+            int i = GetPrivateProfileString(section, key, "", sb, 255, this.path);
+            return sb.ToString();
+        }
+
+        public void Dispose(){}
+    }
+}
